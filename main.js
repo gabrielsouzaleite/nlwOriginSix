87c348135650494d5f54ed1eb73a606f9d5a3d8c
Gabriel Souza Leite <78801605+gabrielsouzaleite@users.noreply.github.com>
2021-12-21 22:53:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,75 @@
+//OPEN AND CLOSE MENU
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+//CLOSE MENU WHEN IT IS OPEN
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+//SHADOW AFTER SCROLL
+function shadowAfterScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+}
+
+//SWIPER
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+//SCROLL REVEAL
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
+
+//BACK TO TOP BUTTON
+
+window.addEventListener('scroll', function () {
+  shadowAfterScroll()
+  backToTop()
+})
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
